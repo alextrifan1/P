625092a5a021fb411bd35475a0657f829686f8fb
alextrifan1 <alexandrutrifan17@gmail.com>
2024-02-19 13:27:52 +0200

    instructiuni

--- CarSorting/main.py
+++ CarSorting/main.py
@@ -7,5 +7,13 @@ if __name__ == '__main__':
 
     car_serivce = CarService(car_repo)
     console = CarConsole(car_serivce)
+    
+    #toate comenzile
+
+    #PRINT all
+    #SEARCH token
+    #BSEARCH token
+    #SORT {token, markModel, markModelToken, profit}
+    #QUICKSORT {token, markModel, markModelToken, profit}
 
     console.run_car_console()
